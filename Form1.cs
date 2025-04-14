7e390958b1c3ea168000161868e341af998c9ac1
Grishin Alexandr <klyaksa945@gmail.com>
2025-04-14 13:02:15 +0300

    CLOSE #3

--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,37 @@ namespace Examen
 {
     public partial class Form1 : Form
     {
+        private bool pwd;
         public Form1()
         {
             InitializeComponent();
+            pwd = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "user" && textBox2.Text == "user")
+            {
+                //Form Main = new Main();
+            }
+            else
+            {
+                MessageBox.Show("Неверный логин или пароль!", "Ошибка входа", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pwd)
+            {
+                pwd = false;
+                textBox2.PasswordChar = '*';
+            }
+            else
+            {
+                pwd = true;
+                textBox2.PasswordChar = '\0';
+            }
         }
     }
 }
